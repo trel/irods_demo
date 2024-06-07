8b6901ea7bd5be09f93641c01c4d8619ce47d9e2
Terrell Russell <terrellrussell@gmail.com>
2024-06-07 19:54:06 -0400

    x seconds rule, default of one day

--- migration.py
+++ migration.py
@@ -1,3 +1,12 @@
+#################################
+# add this one line to core.py
+#################################
+#
+# from migration import *
+#
+#################################
+
+import time
 from genquery import *
 
 ############
@@ -10,7 +19,7 @@ gcp_collection = '/otherZone/home/public'
 
 ############
 
-delay_condition = '<PLUSET>1s</PLUSET><INST_NAME>irods_rule_engine_plugin-{0}-instance</INST_NAME>'
+delay_condition = '<PLUSET>{0}s</PLUSET><INST_NAME>irods_rule_engine_plugin-{1}-instance</INST_NAME>'
 
 ############
 
@@ -19,7 +28,7 @@ def migration_add_sweeper_to_queue(rule_args, callback, rei):
     global delay_condition
     ruletext = 'callback.migration_sync_all_hydroshare_resources();'
     print('queuing sweeper', ruletext)
-    callback.delayExec(delay_condition.format('python')+'<EF>REPEAT FOR EVER</EF>', ruletext, '')
+    callback.delayExec(delay_condition.format('86400', 'python')+'<EF>REPEAT FOR EVER</EF>', ruletext, '')
 
 # Enqueue a sync for each hydroshare resource (all of them)
 def migration_sync_all_hydroshare_resources(rule_args, callback, rei):
@@ -34,7 +43,7 @@ def migration_sync_all_hydroshare_resources(rule_args, callback, rei):
         hydroshare_resource_name = path_to_sync.split('/')[-1]
         ruletext = 'callback.msiCollRsync("{0}", "{1}", "null", "IRODS_TO_IRODS", 0);'.format(path_to_sync, gcp_collection+"/"+hydroshare_resource_name)
         print('queuing resource', ruletext)
-        callback.delayExec(delay_condition.format('python'), ruletext, '')
+        callback.delayExec(delay_condition.format('1', 'python'), ruletext, '')
 
 # Enqueue the sync of a single resource upon request
 def migration_sync_single_hydroshare_resource(rule_args, callback, rei):
@@ -44,5 +53,26 @@ def migration_sync_single_hydroshare_resource(rule_args, callback, rei):
     hydroshare_resource_name = path_to_sync.split('/')[-1]
     ruletext = 'callback.msiCollRsync("{0}", "{1}", "null", "IRODS_TO_IRODS", 0);'.format(path_to_sync, gcp_collection+"/"+hydroshare_resource_name)
     print('queuing single resource', ruletext)
-    callback.delayExec(delay_condition.format('python'), ruletext, '')
+    callback.delayExec(delay_condition.format('1', 'python'), ruletext, '')
 
+# Enqueue a sync for each file newer than X seconds
+def migration_sync_all_files_newer_than_x_seconds(rule_args, callback, rei):
+    global delay_condition
+    global gcp_collection
+    try:
+        seconds_ago = rule_args[0]
+    except IndexError:
+        seconds_ago = 86400 # default, 1 day
+    epoch_seconds_for_genquery = '0'+str(int(time.time() - int(seconds_ago)))
+    callback.writeLine('serverLog', 'seconds_ago [{0}]'.format(seconds_ago))
+    for result in row_iterator("COLL_NAME, DATA_NAME",
+                               "DATA_MODIFY_TIME > '{0}'".format(epoch_seconds_for_genquery),
+                               AS_LIST,
+                               callback):
+        path_to_sync = '{0}/{1}'.format(result[0], result[1])
+        parts = path_to_sync.split('/')
+        parts[1] = gcp_collection.split('/')[1]
+        target_path = '/'.join(parts)
+        ruletext = 'callback.msiDataObjRsync("{0}", "IRODS_TO_IRODS", "null", "{1}", 0);'.format(path_to_sync, target_path)
+        callback.writeLine('serverLog', 'queuing data object [{0}]'.format(ruletext))
+        callback.delayExec(delay_condition.format('1', 'python'), ruletext, '')
